e3aae60359f2c76610760d7572a736179c5a4a84
malgo <malgoo.dev@gmail.com>
2025-09-30 18:26:35 +0700

    fix: fix gizmos leaking memory

--- Utilities/CustomGizmos.cs
+++ Utilities/CustomGizmos.cs
@@ -6,20 +6,36 @@ namespace Malgo.Utilities
 {
     public class CustomGizmos
     {
-        public static void DrawText(Vector3 position, string text, Color textColor, float backgroundAlpha = 1f)
-        {
-            GUIStyle style = new GUIStyle(GUI.skin.label);
+        private static Texture2D backgroundTexture;
 
-            style.normal.textColor = textColor;
+            public static void DrawText(Vector3 position, string text, Color textColor, float backgroundAlpha = 1f)
+            {
+#if UNITY_EDITOR
+                if (backgroundTexture == null)
+                {
+                    backgroundTexture = new Texture2D(1, 1, TextureFormat.RGBA32, false)
+                    {
+                        wrapMode = TextureWrapMode.Clamp,
+                        filterMode = FilterMode.Point
+                    };
+                }
 
-            Texture2D backgroundTexture = new Texture2D(1, 1);
-            backgroundTexture.SetPixel(0, 0, new Color(0f, 0f, 0f, backgroundAlpha));
-            backgroundTexture.Apply();
-            style.normal.background = backgroundTexture;
+                // Update pixel color only if needed
+                backgroundTexture.SetPixel(0, 0, new Color(0f, 0f, 0f, backgroundAlpha));
+                backgroundTexture.Apply();
 
-            Handles.Label(position, text, style);
-        }
+                var style = new GUIStyle(GUI.skin.label)
+                {
+                    normal =
+                    {
+                        textColor = textColor,
+                        background = backgroundTexture
+                    }
+                };
 
+                Handles.Label(position, text, style);
+#endif
+            }
         public static void DrawWireSphere(Vector3 position, float radius, Color color)
         {
             Handles.color = color;
